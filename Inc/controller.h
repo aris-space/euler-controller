258b9a4af6e05f03397c5e25378e1894288ceaf2
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-10-04 12:03:23 +0200

    added control_step method to controller.c

--- Inc/controller.h
+++ Inc/controller.h
@@ -11,20 +11,21 @@
 #include <string.h>
 #include <stdbool.h>
 #include "../../aris-euler-state-estimation/Inc/flight_phase_detection.h"
+#include "../../aris-euler-state-estimation/Inc/state_est_const.h"
 #include "controller_const.h"
 
 
 /* function declaration */
-void compute_control_input(control_data_t *control_data, flight_phase_detection_t *current_flight_phase_detection);
-void control_data_reset(control_data_t *control_data);
 void control_data_init(control_data_t *control_data);
+void control_data_reset(control_data_t *control_data);
+void control_step(control_data_t *control_data, state_est_data_t *state_est_data, flight_phase_detection_t *flight_phase_detection);
+void compute_control_input(control_data_t *control_data, flight_phase_detection_t *current_flight_phase_detection);
 void evaluate_polyfit(control_data_t *control_data);
 void compute_antiwindup_boundaries(control_data_t *control_data);
 void compute_reference_error(control_data_t *control_data);
-void check_apogee_approach_phase(control_data_t *control_data, flight_phase_detection_t *flight_phase_detection);
+void compute_test_control_input(control_data_t *control_data);
 #ifdef EULER_SIMCON
 void save_evaluated_polyfits_to_file(control_data_t *control_data);
 #endif
-void compute_test_control_input(control_data_t *control_data);
 
 #endif //C_IMPLEMENTATION_CONTROLLER_H
