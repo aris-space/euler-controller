88405bea58e8a86477098a4f1cb59b76b5c9ec56
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-10-05 11:09:53 +0200

    added x0 to mpc control step

--- Src/mpc_controller.c
+++ Src/mpc_controller.c
@@ -18,12 +18,18 @@ void compute_control_input(control_data_t *control_data, flight_phase_detection_
         float Bd[2] = {0};
         plant_linearization(control_data, flight_phase_detection, Ad, Bd);
 
+        float x0[3] = {0, control_data->reference_error, control_data->integrated_error};
         float C[3][4] = {{0, 1, 0, 0}, {0, 0, Ad[0][0], Ad[0][1]}, {0, 0, Ad[1][0], Ad[1][1]}};
         float D[3][4] = {{1, -1, 0, 0}, {0, Bd[0], -1, 0}, {0, Bd[1], 0, -1}};
-        float cost_H[4][4] = {{control_data->R, 0, 0, 0}, 
-                              {0, control_data->Q[0][0], control_data->Q[0][1], control_data->Q[0][2]}, 
-                              {0, control_data->Q[1][0], control_data->Q[1][1], control_data->Q[1][2]}, 
-                              {0, control_data->Q[2][0], control_data->Q[2][1], control_data->Q[2][2]}};
+        float cost_H_fin[4][4] = {{control_data->R, 0, 0, 0}, 
+                                 {0, control_data->Q[0][0], control_data->Q[0][1], control_data->Q[0][2]}, 
+                                 {0, control_data->Q[1][0], control_data->Q[1][1], control_data->Q[1][2]}, 
+                                 {0, control_data->Q[2][0], control_data->Q[2][1], control_data->Q[2][2]}};
+
+        memcpy(&control_data->mpc_params.eq_c, &x0, sizeof(x0));
+        memcpy(&control_data->mpc_params.eq_C, &C, sizeof(C));
+        memcpy(&control_data->mpc_params.eq_D, &D, sizeof(D));
+        memcpy(&control_data->mpc_params.cost_H_fin, &cost_H_fin, sizeof(cost_H_fin));
 
         MPC_embotech_single_integrator_test_20201004181950_maximilianstoelzle_solve(&control_data->mpc_params, &control_data->mpc_output, 
                                                                                     &control_data->mpc_info, NULL);
